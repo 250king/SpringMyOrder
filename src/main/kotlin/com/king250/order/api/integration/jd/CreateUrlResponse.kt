c0686fd311bb0dd331a7e9011c475ea09a0b729f
Cheng Fuyuan <chengfuyuan888@gmail.com>
2026-03-18 14:28:40 +0800

    Fix name problem

--- src/main/kotlin/com/king250/order/api/integration/jd/CreateUrlResponse.kt
+++ src/main/kotlin/com/king250/order/api/integration/jd/CreateUrlResponse.kt
@@ -3,7 +3,7 @@ package com.king250.order.api.integration.jd
 import com.fasterxml.jackson.databind.PropertyNamingStrategies
 import com.fasterxml.jackson.databind.annotation.JsonNaming
 
-@JsonNaming(PropertyNamingStrategies.LowerCaseStrategy::class)
+@JsonNaming(PropertyNamingStrategies.LowerCamelCaseStrategy::class)
 data class CreateUrlResponse(
     val url: String,
 )
--- src/main/kotlin/com/king250/order/api/integration/kd100/Kd100Response.kt
+++ src/main/kotlin/com/king250/order/api/integration/kd100/Kd100Response.kt
@@ -3,7 +3,7 @@ package com.king250.order.api.integration.kd100
 import com.fasterxml.jackson.databind.PropertyNamingStrategies
 import com.fasterxml.jackson.databind.annotation.JsonNaming
 
-@JsonNaming(PropertyNamingStrategies.LowerCaseStrategy::class)
+@JsonNaming(PropertyNamingStrategies.LowerCamelCaseStrategy::class)
 data class Kd100Response<T>(
     val result: Boolean,
 
